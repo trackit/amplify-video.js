7d6f32f85edc8ad61dca84c6a26e98ab84337533
Nathan AGEZ <nathan.agez@epitech.eu>
2021-04-13 14:24:34 +0200

    chore(*): Base commit

---
+++ scripts/post-install.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+
+console.log(chalk.green('------------------------------------'));
+console.log('\n');
+console.log(chalk.green('Successfully installed amplify-video.js'));
+console.log('\n');
+console.log(chalk.green('------------------------------------'));
+console.log('\n');
