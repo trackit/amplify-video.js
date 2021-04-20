6f9b493fd6c0a9ffd47a3b9d5ee8c1a9d858b097
Nathan AGEZ <nathan.agez@epitech.eu>
2021-04-20 14:39:30 +0200

    feat(module): Modularize video lib

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
+import { ConsoleLogger as Logger } from '@aws-amplify/core';
 import VideoClass from './src/video';
+
+const logger = new Logger('Video');
 
 let instance: VideoClass = null;
 
 if (!instance) {
+  logger.debug('Create Video Instance');
   instance = new VideoClass();
 }
 
