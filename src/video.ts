69e73c27a708ea683eaf235f8184f4e8fd66daf7
Nathan AGEZ <nathan.agez@epitech.eu>
2021-04-20 11:42:58 +0200

    refactor(video): Add put & delete methods

--- src/video.ts
+++ src/video.ts
@@ -1,13 +1,37 @@
-export default class Video {
-  private test: string = 'test string';
+/* eslint-disable @typescript-eslint/lines-between-class-members */
+import Amplify, { Storage } from 'aws-amplify';
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  upload(_videoFileReferance: string, _metadadict: any) {
-    return this.test;
+export default class VideoClass {
+  private videoConfig = null;
+  private amplifyConfig = null;
+
+  public configure(config, videoConfig) {
+    this.amplifyConfig = Amplify.configure(config);
+    this.videoConfig = videoConfig;
+    console.log(Amplify);
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  delete(_vodAssetVideoId: string) {
-    return this.test;
+  public async upload(file, config?) {
+    config = {
+      bucket: this.videoConfig.awsInputVideo,
+      region: this.amplifyConfig.aws_project_region,
+      customPrefix: {
+        public: '',
+      },
+      ...config,
+    };
+    // -> TODO: GraphQL
+    return Storage.put(file.name, file, config);
+  }
+
+  public async delete(asset: string, config?) {
+    config = {
+      bucket: this.videoConfig.awsInputVideo,
+      region: this.amplifyConfig.aws_project_region,
+      ...config,
+    };
+
+    // TODO: GraphQL
+    return Storage.remove(asset, config);
   }
 }
