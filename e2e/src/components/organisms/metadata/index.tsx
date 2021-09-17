0469607dc3a7c80bfc64da203aae377f067b4f60
Nathan <nathan.agez@epitech.eu>
2021-09-17 18:39:48 +0200

    feat(metadata): Add metadata component

--- e2e/src/components/organisms/metadata/index.tsx
+++ e2e/src/components/organisms/metadata/index.tsx
@@ -8,7 +8,6 @@ import {
 import Block from 'components/molecules/block';
 import Input from 'components/atoms/input';
 import Video from 'amplify-video.js/dist';
-import awsvideoconfig from 'aws-video-exports';
 
 interface Props {
   children?: ReactNode;
@@ -19,45 +18,31 @@ interface Props {
   callback?: any;
 }
 
-const Upload = (props: Props) => {
+const Metadata = (props: Props) => {
   const inputRef = useRef() as MutableRefObject<HTMLInputElement>;
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = async () => {
+    if (!inputRef.current.value) return;
     setLoading(true);
-    if (!inputRef.current.files) return;
-    const bucket = awsvideoconfig.awsInputVideo;
-    const file = inputRef.current.files[0];
-    const data = await Video.upload(
-      file,
-      { description: 'e2e test from Cypress', title: 'Upload method test' },
-      {
-        bucket,
-        contentType: 'video/*',
-        progressCallback: (progress: any) => {
-          const { loaded, total } = progress;
-          const percent = Math.round((loaded / total) * 100);
-          console.log(`Uploaded: ${percent}%`);
-        },
-      },
-    );
+    const vodAssetVideoId = inputRef.current.value;
+    const { data } = await Video.metadata(vodAssetVideoId);
     props.callback(data);
     setLoading(false);
-    console.log('data', data);
   };
 
   return (
     <Block
       className={props.className}
-      title="Upload"
-      button="Upload file"
+      title="Metadata"
+      button="Fetch"
       disabled={loading}
       onClick={async () => await handleSubmit()}
     >
       {props.children}
-      <Input type="file" disabled={loading} forwardref={inputRef} />
+      <Input type="text" disabled={loading} forwardref={inputRef} />
     </Block>
   );
 };
 
-export default Upload;
+export default Metadata;
